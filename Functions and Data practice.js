8639921009d27d126264dc438122a98cdcf1c454
inhelliburn89 <79674181+inhelliburn89@users.noreply.github.com>
2021-03-25 21:48:26 -0600

    Update Functions and Data practice.js

--- Functions and Data practice.js
+++ Functions and Data practice.js
@@ -33,3 +33,36 @@ getCredentials(user);
 
 //Define a function checkProperty() that will use the following object passed as an argument to print the output in the console:
 
+let property = {
+  owner: {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 44
+  },
+  isForSale: true,
+  sqrm: 120,
+  address: {
+    street: 'Happy St',
+    number: 123,
+    city: 'Miami',
+    state: 'FL',
+    country: 'US'
+  },
+  amenities: ['pool', 'tennis court', 'private parking', 'yard']
+};
+ 
+// YOUR CODE HERE
+
+
+function checkProperty(){
+     //I first copied and pasted the full text without calling the properties, then found out that it was too easy to do that :c.
+ if (property.isForSale === true){
+  console.log(`The owner, ${property.owner.firstName} ${property.owner.lastName} put the home for sale! The property has ${property.amenities.length} amenities.`);
+ }
+  else {
+    console.log(`The home in ${property.address.street} no. ${property.address.number} is not for sale.`);
+  }
+  
+}
+checkProperty(property);
+
